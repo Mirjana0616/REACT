691f785de74a5df523cf7c6c9a8c033c0117eebb
Dusankol <dusank89@gmail.com>
2020-03-03 19:52:09 +0100

    Initial react structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import "./App.scss";
+
+class App extends Component {
+
+	render() {
+		return (
+			<div>This is some REACT app</div>
+			)
+	}
+
+}
+
+export default App;
